240ca93d7043861ff23aa0a92053f297a7bac0c1
RezaAshrafi77 <ashrafimirreza98@gmail.com>
2024-11-28 15:21:31 +0330

    build(deps): update package-lock.json, package.json and src/components/Button/index.jsx

---
+++ src/components/Label/index.jsx
@@ -0,0 +1,69 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react"
+import { FaRegQuestionCircle } from "react-icons/fa"
+import { LiaArchiveSolid } from "react-icons/lia"
+import { IoIosInformationCircle } from "react-icons/io"
+import Modal from "../Modal"
+import ArchiveTable from "@/components/layouts/ArchiveTable"
+import styles from "./styles.module.css" // Import your CSS module
+
+export default function Label({
+    label,
+    required,
+    className,
+    userGuide,
+    archive,
+    educationalContent,
+}) {
+    const [openModal, setOpenModal] = useState({
+        userGuide: false,
+        archive: false,
+    })
+
+    const handleModalToggle = (type) => {
+        setOpenModal((prev) => ({ ...prev, [type]: !prev[type] }))
+    }
+
+    return (
+        <label className={`${styles.label} ${className}`}>
+            {label}
+            {required && <span className={styles.required}>*</span>}
+
+            {userGuide && (
+                <FaRegQuestionCircle
+                    className={`${styles.guideIcon} ${styles.iconSize} ${styles.hoverGreen} cursor-pointer`}
+                    onClick={() => handleModalToggle("userGuide")}
+                    title="راهنما"
+                />
+            )}
+
+            {archive && (
+                <LiaArchiveSolid
+                    className={`${styles.iconSize} ${styles.hoverGreen} cursor-pointer`}
+                    onClick={() => handleModalToggle("archive")}
+                    title="آرشیو"
+                />
+            )}
+
+            {educationalContent?.show && (
+                <IoIosInformationCircle
+                    className={`${styles.iconSize} ${styles.textGray} ${styles.hoverRed} cursor-pointer`}
+                    onClick={educationalContent.action}
+                />
+            )}
+
+            <Modal
+                isOpen={openModal.userGuide || openModal.archive}
+                onClose={() =>
+                    setOpenModal({ userGuide: false, archive: false })
+                }
+                containerClassName={styles.modalContainer}
+            >
+                {openModal.userGuide ? userGuide : null}
+                {openModal.archive ? (
+                    <ArchiveTable questionKey={archive} />
+                ) : null}
+            </Modal>
+        </label>
+    )
+}
