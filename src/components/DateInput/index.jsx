78c909e82e5b0ec096568a018000e31dc1d3f3d7
RezaAshrafi77 <ashrafimirreza98@gmail.com>
2024-11-30 14:47:58 +0330

    add DateInput

---
+++ src/components/DateInput/index.jsx
@@ -0,0 +1,98 @@
+/* eslint-disable react/prop-types */
+import classNames from "classnames"
+import { Controller } from "react-hook-form"
+import DatePicker from "react-multi-date-picker"
+import persian from "react-date-object/calendars/persian"
+import persian_fa from "react-date-object/locales/persian_fa"
+import { BsCalendar2Event } from "react-icons/bs"
+import { BiError } from "react-icons/bi"
+import Label from "../Label"
+import styles from "./styles.module.css"
+
+const DateInput = ({
+    label,
+    id,
+    control,
+    containerClassName,
+    className,
+    required = false,
+    errors = {},
+    watch,
+    labelClassName,
+    placeholder = "روز/ ماه/ سال",
+    format = "YYYY/MM/DD",
+    calendar = persian,
+    locale = persian_fa,
+    icon = <BsCalendar2Event />,
+    archive,
+}) => {
+    const isError = !!errors[id]
+    const errorMessage = "این سوال اجباری است."
+
+    return (
+        <div
+            className={classNames(
+                styles.container,
+                containerClassName,
+                isError && "field-error"
+            )}
+        >
+            {/* Label */}
+            <Label
+                label={label}
+                required={required}
+                className={labelClassName}
+                archive={archive ? { ...archive, questionKey: id } : false}
+            />
+
+            {/* Input Field */}
+            <div className={classNames(className, "group", styles.field)}>
+                <Controller
+                    control={control}
+                    name={id}
+                    rules={{ required }}
+                    render={({ field: { onChange } }) => (
+                        <DatePicker
+                            inputClass={styles.dateInput}
+                            placeholder={placeholder}
+                            calendar={calendar}
+                            locale={locale}
+                            format={format}
+                            containerClassName={styles.dateInputContainer}
+                            calendarPosition="top"
+                            fixMainPosition={false}
+                            fixRelativePosition={false}
+                            showOtherDays
+                            value={watch(id) || ""}
+                            onChange={(date) =>
+                                onChange(date ? date.toString() : "")
+                            }
+                            id={id}
+                        />
+                    )}
+                />
+                {/* Calendar Icon */}
+                {icon && (
+                    <span
+                        className={classNames(
+                            styles.icon,
+                            "group-hover:text-green text-gray-500 inline"
+                        )}
+                    >
+                        {icon}
+                    </span>
+                )}
+            </div>
+
+            {/* Error Message */}
+            {isError && (
+                <span className="text-error">
+                    <BiError className="text-xs lg:text-base" />
+                    {errorMessage}
+                </span>
+            )}
+        </div>
+    )
+}
+
+export default DateInput
