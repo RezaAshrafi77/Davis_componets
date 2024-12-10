7a6f0d7c6e92199f69c2f5d3a1a9055aa20eea4f
RezaAshrafi77 <ashrafimirreza98@gmail.com>
2024-12-10 16:17:38 +0330

    update 12 files

--- src/pages/SpecialistDashboardPage/index.jsx
+++ src/pages/SpecialistDashboardPage/index.jsx
@@ -1,140 +1,141 @@
 /* eslint-disable react/prop-types */
-import { useEffect, useMemo, useState } from "react"
-import Table from "../../components/Table"
-import Header from "./layouts/Header"
-import { tableSizeList } from "../../components/Table/data"
-import { Table_Columns } from "./layouts/Header/data"
-import { useForm } from "react-hook-form"
-import styles from "./styles.module.css"
-import Radio from "../../components/Radio"
-import moment from "jalali-moment"
+import { useEffect, useMemo, useState } from "react";
+import Table from "../../components/Table";
+import Header from "./layouts/Header";
+import { tableSizeList } from "../../components/Table/data";
+import { Table_Columns } from "./layouts/Header/data";
+import { useForm } from "react-hook-form";
+import styles from "./styles.module.css";
+import Radio from "../../components/Radio";
+import moment from "jalali-moment";
 
 export default function SpecialistDashboardPage({
-    tableColumns = Table_Columns,
-    getUsers = () => {},
-    title,
-    users,
-    loading,
-    colFilter,
-    colors,
-    offset,
+  tableColumns = Table_Columns,
+  getUsers = () => {},
+  title,
+  users,
+  loading,
+  colFilter,
+  colors,
+  offset,
 }) {
-    const { register, watch, control, handleSubmit, setValue } = useForm({
-        mode: "all",
-    })
-    const [currentPage, setCurrentPage] = useState(1)
-    const [tableSize, setTableSize] = useState(tableSizeList[0].value)
-    const [activeFilterOption, setAFO] = useState(null)
+  const { register, watch, control, handleSubmit, setValue } = useForm({
+    mode: "all",
+  });
+  const [currentPage, setCurrentPage] = useState(1);
+  const [tableSize, setTableSize] = useState(tableSizeList[0].value);
+  const [activeFilterOption, setAFO] = useState(null);
 
-    const rows = useMemo(
-        () =>
-            activeFilterOption
-                ? users
-                      ?.map((row, index) => [
-                          offset + index + 1,
-                          row["fn"],
-                          row["6620"],
-                          row["4946"],
-                          moment(row["date_paziresh"])
-                              .locale("fa")
-                              .format("HH:MM - YYYY/MM/DD"),
-                          row["vazeiat"],
-                          row["doctor_fn"],
-                      ])
-                      .filter((row) => row[colFilter - 1] == activeFilterOption)
-                : users?.map((row, index) => [
-                      offset + index + 1,
-                      row["fn"],
-                      row["6620"],
-                      row["4946"],
-                      moment(row["date_paziresh"])
-                          .locale("fa")
-                          .format("HH:MM - YYYY/MM/DD"),
-                      row["vazeiat"],
-                      row["doctor_fn"],
-                  ]),
-        [users, offset]
-    )
+  const rows = useMemo(
+    () =>
+      activeFilterOption
+        ? users
+            ?.map((row, index) => [
+              offset + index + 1,
+              row["fn"],
+              row["6620"],
+              row["4946"],
+              moment(row["date_paziresh"])
+                .locale("fa")
+                .format("HH:MM - YYYY/MM/DD"),
+              row["vazeiat"],
+              row["doctor_fn"],
+            ])
+            .filter((row) => row[colFilter - 1] == activeFilterOption)
+        : users?.map((row, index) => [
+            offset + index + 1,
+            row["fn"],
+            row["6620"],
+            row["4946"],
+            moment(row["date_paziresh"])
+              .locale("fa")
+              .format("HH:MM - YYYY/MM/DD"),
+            row["vazeiat"],
+            row["doctor_fn"],
+          ]),
+    [users, offset, colFilter]
+  );
 
-    useEffect(() => {
-        setValue("from_date", moment().locale("fa").format("YYYY/MM/DD"))
-        setValue("end_date", moment().locale("fa").format("YYYY/MM/DD"))
-    }, [])
+  useEffect(() => {
+    setValue("from_date", moment().locale("fa").format("YYYY/MM/DD"));
+    setValue("end_date", moment().locale("fa").format("YYYY/MM/DD"));
+  }, []);
 
-    useEffect(() => {
-        onSubmit()
-    }, [tableSize, currentPage])
+  useEffect(() => {
+    onSubmit();
+  }, [tableSize, currentPage]);
 
-    const onSubmit = () => {
-        const formData = {
-            6620: watch("6620") || null,
-            from_date: watch("from_date") || null,
-            end_date: watch("end_date") || null,
-            limit: tableSize,
-            offset: Number(tableSize * (Number(currentPage) - 1)),
-        }
-        getUsers({ formData })
-        setCurrentPage(1)
-    }
+  const onSubmit = () => {
+    const formData = {
+      6620: watch("6620") || null,
+      from_date: watch("from_date") || null,
+      end_date: watch("end_date") || null,
+      limit: tableSize,
+      offset: Number(tableSize * (Number(currentPage) - 1)),
+    };
+    getUsers({ formData });
+    setCurrentPage(1);
+  };
 
-    const filterOptions =
-        colFilter && users?.length
-            ? [...new Set(rows?.map((row) => row[colFilter - 1]))]
-            : []
+  const filterOptions =
+    colFilter && users?.length
+      ? [...new Set(rows?.map((row) => row[colFilter - 1]))]
+      : [];
 
-    return (
-        <div className="flex flex-col flex-1 gap-4">
-            <Header
-                loading={loading}
-                watch={watch}
-                register={register}
-                control={control}
-                handleSubmit={handleSubmit}
-                onSubmit={onSubmit}
-                title={title}
-            />
-            {rows?.length ? (
-                <Table
-                    columns={tableColumns}
-                    rows={rows}
-                    page={currentPage}
-                    className={styles.table}
-                    setPage={setCurrentPage}
-                    setTableSize={setTableSize}
-                    colFilter={colFilter ? colFilter - 1 : null}
-                    colors={colors}
-                    tableSize={tableSize}
-                    pagination
-                    containerClassName={loading ? "blur-sm" : ""}
-                >
-                    {colFilter && filterOptions?.length > 1 ? (
-                        <div className="flex items-center h-full pr-2 gap-4">
-                            <span className="text-2xs lg:text-xs font-700">
-                                نمایش بر اساس :
-                            </span>
-                            <div className="flex items-center gap-4">
-                                {filterOptions.map((o) => (
-                                    <Radio
-                                        key={o}
-                                        label={o}
-                                        className={styles.filterRadio}
-                                        name={"filter"}
-                                        value={o}
-                                        checked={o == activeFilterOption}
-                                        onClick={() => {
-                                            if (activeFilterOption == o) {
-                                                setAFO(null)
-                                            } else {
-                                                setAFO(o)
-                                            }
-                                        }}
-                                    />
-                                ))}
-                            </div>
-                        </div>
-                    ) : null}
-                </Table>
-            ) : null}
-        </div>
-    )
+  return (
+    <div className="flex flex-col flex-1 gap-4">
+      <Header
+        loading={loading}
+        watch={watch}
+        register={register}
+        control={control}
+        handleSubmit={handleSubmit}
+        onSubmit={onSubmit}
+        title={title}
+      />
+      {rows?.length ? (
+        <Table
+          columns={tableColumns}
+          rows={rows}
+          page={currentPage}
+          className={styles.table}
+          setPage={setCurrentPage}
+          setTableSize={setTableSize}
+          colFilter={colFilter ? colFilter - 1 : null}
+          colors={colors}
+          tableSize={tableSize}
+          pagination
+          containerClassName={loading ? "blur-sm" : ""}
+        >
+          {colFilter && filterOptions?.length > 1 ? (
+            <div className="flex items-center h-full pr-2 gap-4">
+              <span className="text-2xs lg:text-xs font-700">
+                نمایش بر اساس :
+              </span>
+              <div className="flex items-center gap-4">
+                {filterOptions.map((o) => (
+                  <Radio
+                    key={o}
+                    label={o}
+                    className={styles.filterRadio}
+                    name={"filter"}
+                    value={o}
+                    checked={o == activeFilterOption}
+                    onClick={() => {
+                      if (activeFilterOption == o) {
+                        setAFO(null);
+                      } else {
+                        setAFO(o);
+                      }
+                    }}
+                    onChange={(e) => e}
+                  />
+                ))}
+              </div>
+            </div>
+          ) : null}
+        </Table>
+      ) : null}
+    </div>
+  );
 }
--- src/pages/SpecialistFormPage/index.jsx
+++ src/pages/SpecialistFormPage/index.jsx
@@ -1,41 +1,24 @@
-import React, { useState } from "react";
-import Page from "../../components/Page";
-import { useForm } from "react-hook-form";
-import FileField from "../../components/FileField";
+// import React, { useState } from "react";
+// import Page from "../../components/Page";
+// import { useForm } from "react-hook-form";
+// import { links } from "./data";
 
-export default function SFP() {
-  const {
-    register,
-    watch,
-    handleSubmit,
-    setValue,
-    control,
-    formState: { errors },
-  } = useForm({
-    mode: "all",
-    defaultValues: {},
-  });
+// export default function SFP() {
+//   const {
+//     register,
+//     watch,
+//     handleSubmit,
+//     setValue,
+//     control,
+//     formState: { errors },
+//   } = useForm({
+//     mode: "all",
+//     defaultValues: {},
+//   });
 
-  const [file, setFile] = useState(
-    "/resource/files/ecxhccvpbiublkhpseqaiskuphgdfjqe1733721474328.png"
-  );
+//   const [file, setFile] = useState(
+//     "/resource/files/ecxhccvpbiublkhpseqaiskuphgdfjqe1733721474328.png"
+//   );
 
-  return (
-    <Page>
-      <FileField
-        label="یک فایل را انتخاب کنید."
-        register={register}
-        errors={errors}
-        setValue={setValue}
-        watch={watch}
-        questionKey={"3423"}
-        archive={{}}
-        userGuide={<div>reza</div>}
-        educationalContent={{
-          show: true,
-          type: "danger",
-        }}
-      />
-    </Page>
-  );
-}
+//   return <Page router routes={links} />;
+// }
