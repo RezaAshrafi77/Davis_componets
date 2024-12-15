e58aed9137d43577b8c48c5aca684f4669c341d3
RezaAshrafi77 <ashrafimirreza98@gmail.com>
2024-12-15 23:45:47 +0330

    update 9 files and delete 1 file

--- src/pages/SpecialistFormPage/index.jsx
+++ src/pages/SpecialistFormPage/index.jsx
@@ -1,49 +1,50 @@
-// import FieldSet from "../../components/FieldSet";
-import { useForm } from "react-hook-form"
-import RadioOptions from "../../components/RadioOptions"
+// // import FieldSet from "../../components/FieldSet";
+// import { useForm } from "react-hook-form"
+// import RadioOptions from "../../components/RadioOptions"
 
-export default function SFP() {
-    const { register, watch, control, handleSubmit, setValue } = useForm({
-        mode: "all",
-    })
+// export default function SFP() {
+//     const { register, watch, control, handleSubmit, setValue } = useForm({
+//         mode: "all",
+//     })
 
-    return (
-        <>
-            <RadioOptions
-                containerClassName="!w-[500px]"
-                radioClassName="w-[100px]"
-                label="رضا :"
-                divider={"center"}
-                options={[
-                    { label: "1", value: "1" },
-                    { label: "2", value: "2" },
-                    { label: "2", value: "2" },
-                    { label: "2", value: "2" },
-                    { label: "2", value: "2" },
-                    { label: "2", value: "2" },
-                    { label: "2", value: "2" },
-                    { label: "2", value: "2" },
-                    { label: "2", value: "2" },
-                    { label: "2", value: "2" },
-                    { label: "2", value: "2" },
-                    { label: "2", value: "2" },
-                    { label: "2", value: "2" },
-                    { label: "2", value: "2" },
-                    { label: "2", value: "2" },
-                    { label: "2", value: "2" },
-                    { label: "2", value: "2" },
-                    { label: "2", value: "2" },
-                    { label: "2", value: "2" },
-                    { label: "2", value: "2" },
-                    { label: "2", value: "2" },
-                    { label: "2", value: "2" },
-                    { label: "2", value: "2" },
-                    { label: "2", value: "2" },
-                    { label: "2", value: "2" },
-                    { label: "2", value: "2" },
-                    { label: "2", value: "2" },
-                ]}
-            />
-        </>
-    )
-}
+//     return (
+//         <>
+//             <RadioOptions
+//                 containerClassName="!w-[500px]"
+//                 radioClassName="w-[100px]"
+//                 label="رضا :"
+//                 required
+//                 divider={"center"}
+//                 options={[
+//                     { label: "1", value: "1" },
+//                     { label: "2", value: "2" },
+//                     { label: "2", value: "2" },
+//                     { label: "2", value: "2" },
+//                     { label: "2", value: "2" },
+//                     { label: "2", value: "2" },
+//                     { label: "2", value: "2" },
+//                     { label: "2", value: "2" },
+//                     { label: "2", value: "2" },
+//                     { label: "2", value: "2" },
+//                     { label: "2", value: "2" },
+//                     { label: "2", value: "2" },
+//                     { label: "2", value: "2" },
+//                     { label: "2", value: "2" },
+//                     { label: "2", value: "2" },
+//                     { label: "2", value: "2" },
+//                     { label: "2", value: "2" },
+//                     { label: "2", value: "2" },
+//                     { label: "2", value: "2" },
+//                     { label: "2", value: "2" },
+//                     { label: "2", value: "2" },
+//                     { label: "2", value: "2" },
+//                     { label: "2", value: "2" },
+//                     { label: "2", value: "2" },
+//                     { label: "2", value: "2" },
+//                     { label: "2", value: "2" },
+//                     { label: "2", value: "2" },
+//                 ]}
+//             />
+//         </>
+//     )
+// }
