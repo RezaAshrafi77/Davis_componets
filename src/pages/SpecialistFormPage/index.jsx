9f4a225f390f092b7bd8865c40ca677ec94c95c7
RezaAshrafi77 <ashrafimirreza98@gmail.com>
2024-12-19 12:10:21 +0330

    update 4 files and delete 1 file

--- src/pages/SpecialistFormPage/index.jsx
+++ src/pages/SpecialistFormPage/index.jsx
@@ -1,16 +1,23 @@
+// // import FieldSet from "../../components/FieldSet";
+// import { useForm } from "react-hook-form";
+// import Page from "../../components/Page";
 // import FieldSet from "../../components/FieldSet";
-import { useForm } from "react-hook-form";
-import Page from "../../components/Page";
-import FieldSet from "../../components/FieldSet";
-import Header from "../SpecialistDashboardPage/layouts/Header";
-export default function SFP() {
-  const { watch, setValue, control, handleSubmit } = useForm({
-    mode: "all",
-    defaultValues: {},
-  });
-  return (
-    <Page>
-      <Header handleSubmit={handleSubmit} watch={watch} control={control} />
-    </Page>
-  );
-}
+// import Header from "../SpecialistFormPage/layouts/Header";
+// export default function SFP() {
+//   const { watch, setValue, control, handleSubmit } = useForm({
+//     mode: "all",
+//     defaultValues: {},
+//   });
+//   return (
+//     <Page>
+//       <FieldSet title={"reza"}>
+//         <br />
+//         <br />
+//         <br />
+//         <br />
+//         <br />
+//         <br />
+//       </FieldSet>
+//     </Page>
+//   );
+// }
