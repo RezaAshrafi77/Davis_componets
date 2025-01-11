b3cb324a339a2ec7763a4333d889433398174654
RezaAshrafi77 <ashrafimirreza98@gmail.com>
2025-01-11 17:22:07 +0330

    build(deps): update package-lock.json, package.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,32 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import path from "path"
-import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({
-    plugins: [react(), cssInjectedByJsPlugin()],
-    build: {
-        lib: {
-            entry: path.resolve(__dirname, "./src/main.js"),
-            name: "ReactElementsDavis",
-            formats: ["es", "cjs"],
-            fileName: (format) => `main.${format}.js`,
-        },
-        rollupOptions: {
-            external: ["react", "react-dom"],
-            output: {
-                globals: {
-                    react: "React",
-                    "react-dom": "ReactDOM",
-                },
-            },
-        },
+  plugins: [react(), cssInjectedByJsPlugin()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "./src/main.js"),
+      name: "ReactElementsDavis",
+      formats: ["es", "cjs"],
+      fileName: (format) => `main.${format}.js`,
     },
-})
+    rollupOptions: {
+      external: [
+        "react",
+        "react-dom",
+        "react-hook-form",
+        "classnames",
+        "jalali-moment",
+        "react-icons",
+      ],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
