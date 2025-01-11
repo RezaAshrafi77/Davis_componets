bbd75090a7c2b6797d3fc0325e942ffc6a18c0cd
RezaAshrafi77 <ashrafimirreza98@gmail.com>
2025-01-12 00:15:54 +0330

    update 8 files

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,35 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import path from "path"
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "./src/main.js"),
-      name: "ReactElementsDavis",
-      formats: ["es", "cjs"],
-      fileName: (format) => `main.${format}.js`,
-    },
-    target: "esnext", // Target modern JavaScript for smaller and faster bundles
-    minify: "esbuild", // Ensure minification
-    // sourcemap: true, // Generate source maps for better debugging in production
-    cssCodeSplit: true, // Separate CSS for better performance
-    rollupOptions: {
-      external: [
-        "react",
-        "react-dom",
-        "react-hook-form",
-        "classnames",
-        "jalali-moment",
-        "react-icons",
-      ],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+    plugins: [react()],
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, "./src/main.js"),
+            name: "ReactElementsDavis",
+            formats: ["es", "cjs"],
+            fileName: (format) => `main.${format}.js`,
         },
-      },
+        target: "esnext", // Target modern JavaScript for smaller and faster bundles
+        minify: "esbuild", // Ensure minification
+        // sourcemap: true, // Generate source maps for better debugging in production
+        cssCodeSplit: false, // Separate CSS for better performance
+        rollupOptions: {
+            external: [
+                "react",
+                "react-dom",
+                "react-hook-form",
+                "classnames",
+                "jalali-moment",
+                "react-icons",
+            ],
+            output: {
+                globals: {
+                    react: "React",
+                    "react-dom": "ReactDOM",
+                },
+            },
+        },
     },
-  },
-});
+})
