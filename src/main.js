0a356248a426dba1782aae4d9fb0284c02a362ac
RezaAshrafi77 <ashrafimirreza98@gmail.com>
2025-01-14 22:09:48 +0330

    update 7 files and delete 1 file

--- src/main.js
+++ src/main.js
@@ -1,52 +1,52 @@
-import "./index.css";
+import "./index.css"
 // components;
-export { Button } from "./components/Button/index.jsx";
-export { Divider } from "./components/Divider/index.jsx";
-export { Dropdown } from "./components/Dropdown/index.jsx";
-export { HTML } from "./components/HTML/index.jsx";
-export { Label } from "./components/Label/index.jsx";
-export { Modal } from "./components/Modal/index.jsx";
-export { Page } from "./components/Page/index";
-export { PageCard } from "./components/PageCard/index.jsx";
-export { ProjectName } from "./components/ProjectName/index.jsx";
-export { Table } from "./components/Table/index.jsx";
-export { TextField } from "./components/TextField/index.jsx";
-export { Radio } from "./components/Radio/index.jsx";
-export { RadioOptions } from "./components/RadioOptions/index.jsx";
-export { DateInput } from "./components/DateInput/index.jsx";
-export { CheckBox } from "./components/CheckBox/index.jsx";
-export { CheckBoxGroup } from "./components/CheckBoxGroup/index.jsx";
-export { Select } from "./components/Select/index.jsx";
-export { FieldSet } from "./components/FieldSet/index.jsx";
-export { FileField } from "./components/FileField/index.jsx";
+export { Button } from "./components/Button/index.jsx"
+export { Divider } from "./components/Divider/index.jsx"
+export { Dropdown } from "./components/Dropdown/index.jsx"
+export { HTML } from "./components/HTML/index.jsx"
+export { Label } from "./components/Label/index.jsx"
+export { Modal } from "./components/Modal/index.jsx"
+export { Page } from "./components/Page/index"
+export { PageCard } from "./components/PageCard/index.jsx"
+export { ProjectName } from "./components/ProjectName/index.jsx"
+export { Table } from "./components/Table/index.jsx"
+export { TextField } from "./components/TextField/index.jsx"
+export { Radio } from "./components/Radio/index.jsx"
+export { RadioOptions } from "./components/RadioOptions/index.jsx"
+export { DateInput } from "./components/DateInput/index.jsx"
+export { CheckBox } from "./components/CheckBox/index.jsx"
+export { CheckBoxGroup } from "./components/CheckBoxGroup/index.jsx"
+export { Select } from "./components/Select/index.jsx"
+export { FieldSet } from "./components/FieldSet/index.jsx"
+export { FileField } from "./components/FileField/index.jsx"
 // layouts
-export { ArchiveTable } from "./layouts/ArchiveTable/index.jsx";
-export { EmptyForm } from "./layouts/EmptyForm/index.jsx";
-export { Header as SpecialistFormPageHeader } from "./pages/SpecialistFormPage/layouts/Header/index.jsx";
+export { ArchiveTable } from "./layouts/ArchiveTable/index.jsx"
+export { EmptyForm } from "./layouts/EmptyForm/index.jsx"
+export { Header as SpecialistFormPageHeader } from "./pages/SpecialistFormPage/layouts/Header/index.jsx"
 // page
-export { SpecialistDashboardPage } from "./pages/SpecialistDashboardPage/index.jsx";
-export { SpecialistPrintPage } from "./pages/SpecialistPrintPage/index.jsx";
+export { SpecialistDashboardPage } from "./pages/SpecialistDashboardPage/index.jsx"
+export { SpecialistPrintPage } from "./pages/SpecialistPrintPage/index.jsx"
 // sections
-export { Prescription } from "./sections/Prescription/index.jsx";
+export { Prescription } from "./sections/Prescription/index.jsx"
 
-// import ReactDOM from "react-dom/client";
-// // import "./index.css";
-// import { Page } from "./components/Page";
-// import { Header } from "./pages/SpecialistFormPage/layouts/Header";
-// import { useForm } from "react-hook-form";
+// import ReactDOM from "react-dom/client"
+// import "./index.css"
+// import { Page } from "./components/Page"
+// import { Header } from "./pages/SpecialistFormPage/layouts/Header"
+// import { useForm } from "react-hook-form"
 
 // const MyFunction = () => {
-//   const { register, watch, control, setValue } = useForm({
-//     mode: "all",
-//   });
-//   return (
-//     <Page back={true}>
-//       <Header />
-//     </Page>
-//   );
-// };
+//     const { register, watch, control, setValue } = useForm({
+//         mode: "all",
+//     })
+//     return (
+//         <Page back={true}>
+//             <Header />
+//         </Page>
+//     )
+// }
 
 // ReactDOM.createRoot(document.getElementById("root")).render(
-//   //   <StrictMode>
-//   <MyFunction />
-// );
+//     //   <StrictMode>
+//     <MyFunction />
+// )
