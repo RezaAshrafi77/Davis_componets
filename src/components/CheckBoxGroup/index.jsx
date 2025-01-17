685122dc09307d034dd276814ef24c55b7b9ee6e
RezaAshrafi77 <ashrafimirreza98@gmail.com>
2025-01-17 18:37:23 +0330

    update 6 files and delete 1 file

--- src/components/CheckBoxGroup/index.jsx
+++ src/components/CheckBoxGroup/index.jsx
@@ -1,115 +1,121 @@
 /* eslint-disable react/prop-types */
-import classNames from "classnames";
-import { BiError } from "react-icons/bi";
+import classNames from "classnames"
+import { BiError } from "react-icons/bi"
 // import useDevice from "../../hooks/useDevice"
-import { CheckBox } from "../CheckBox";
-import { Divider } from "../Divider";
-import { Label } from "../Label";
-import styles from "./styles.module.css";
-import { useEffect } from "react";
+import { CheckBox } from "../CheckBox"
+import { Divider } from "../Divider"
+import { Label } from "../Label"
+import styles from "./styles.module.css"
+import { useEffect } from "react"
 
 export const CheckBoxGroup = ({
-  containerClassName,
-  optionsContainer,
-  checkBoxClassName,
-  options = [],
-  label,
-  divider = false,
-  labelClassName,
-  educationalContent,
-  userGuide,
-  errors,
-  questionKey,
-  required = false,
-  setValue,
-  errorMessage = "پر کردن این قسمت الزامیست.",
-  watch,
-  errorIcon,
-  archive,
-  dividerClassName,
-  register,
-  labelMore,
-  en,
+    containerClassName,
+    optionsContainer,
+    checkBoxClassName,
+    options = [],
+    label,
+    divider = false,
+    labelClassName,
+    educationalContent,
+    userGuide,
+    errors,
+    questionKey,
+    required = false,
+    setValue,
+    errorMessage = "پر کردن این قسمت الزامیست.",
+    watch,
+    errorIcon,
+    archive,
+    dividerClassName,
+    register,
+    labelMore,
+    en,
 }) => {
-  // const [device] = useDevice()
-  const isError = !!errors?.[questionKey];
-  const selectedValues = (watch?.(questionKey) || []).map((o) => String(o));
+    // const [device] = useDevice()
+    const isError = !!errors?.[questionKey]
+    const selectedValues = (watch?.(questionKey) || []).map((o) => String(o))
 
-  const handleCheckboxChange = (value) => {
-    const updatedValues = selectedValues.includes(String(value))
-      ? selectedValues.filter((v) => v !== String(value))
-      : [...selectedValues, String(value)];
-    setValue?.(questionKey, updatedValues, { shouldValidate: true });
-  };
-
-  const renderCheckBox = (option) => (
-    <CheckBox
-      key={option.value}
-      name={questionKey}
-      value={option.value}
-      label={option.label}
-      checked={selectedValues.includes(option.value)}
-      onChange={() => handleCheckboxChange(option.value)}
-      className={classNames(checkBoxClassName)}
-      en={en}
-    />
-  );
-
-  const labelDirectionStyle = {
-    center: "label-center",
-    right: "label-right",
-    left: "label-left",
-  };
-
-  useEffect(() => {
-    if (required) {
-      register(questionKey, {
-        required,
-      });
+    const handleCheckboxChange = (value) => {
+        const updatedValues = selectedValues.includes(String(value))
+            ? selectedValues.filter((v) => v !== String(value))
+            : [...selectedValues, String(value)]
+        setValue?.(questionKey, updatedValues, { shouldValidate: true })
     }
-  }, []);
-  return (
-    <div
-      className={classNames(
-        "w-full flex flex-col p-2 bg-formItem rounded relative",
-        containerClassName,
-        isError ? "field-error" : ""
-      )}
-      style={{
-        boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.15)",
-      }}
-    >
-      {label && (
-        <Label
-          className={classNames(labelClassName, labelDirectionStyle[divider])}
-          userGuide={userGuide}
-          educationalContent={educationalContent}
-          archive={archive ? { ...archive, questionKey } : false}
-          label={label}
-          required={required}
-          en={en}
-          more={labelMore}
+
+    const renderCheckBox = (option) => (
+        <CheckBox
+            key={option.value}
+            name={questionKey}
+            value={option.value}
+            label={option.label}
+            checked={selectedValues.includes(option.value)}
+            onChange={() => handleCheckboxChange(option.value)}
+            className={classNames(checkBoxClassName)}
+            en={en}
         />
-      )}
-      {divider && (
-        <Divider className={classNames(dividerClassName)} position={divider} />
-      )}
-      <div
-        className={classNames(
-          optionsContainer,
-          styles.options,
-          "flex w-full flex-wrap gap-1.5"
-        )}
-        dir={en ? "ltr" : "rtl"}
-      >
-        {options.map(renderCheckBox)}
-      </div>
-      {isError && (
-        <span className="error">
-          {errorIcon || <BiError className="text-xs lg:text-base" />}
-          {errorMessage}
-        </span>
-      )}
-    </div>
-  );
-};
+    )
+
+    const labelDirectionStyle = {
+        center: "label-center",
+        right: "label-right",
+        left: "label-left",
+    }
+
+    useEffect(() => {
+        if (required) {
+            register(questionKey, {
+                required,
+            })
+        }
+    }, [])
+    return (
+        <div
+            className={classNames(
+                "w-full flex flex-col p-2 bg-formItem rounded relative",
+                containerClassName,
+                isError ? "field-error" : ""
+            )}
+            style={{
+                boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.15)",
+            }}
+        >
+            {label && (
+                <Label
+                    className={classNames(
+                        labelClassName,
+                        labelDirectionStyle[divider]
+                    )}
+                    userGuide={userGuide}
+                    educationalContent={educationalContent}
+                    archive={archive ? { ...archive, questionKey } : false}
+                    label={label}
+                    required={required}
+                    en={en}
+                    more={labelMore}
+                />
+            )}
+            {divider && (
+                <Divider
+                    className={classNames(dividerClassName)}
+                    position={divider}
+                />
+            )}
+            <div
+                className={classNames(
+                    optionsContainer,
+                    styles.options,
+                    "flex w-full flex-wrap gap-1.5"
+                )}
+                dir={en ? "ltr" : "rtl"}
+            >
+                {options.map(renderCheckBox)}
+            </div>
+            {isError && (
+                <span className="error">
+                    {errorIcon || <BiError className="text-xs lg:text-base" />}
+                    {errorMessage}
+                </span>
+            )}
+        </div>
+    )
+}
--- src/components/Select/index.jsx
+++ src/components/Select/index.jsx
@@ -1,211 +1,233 @@
 /* eslint-disable react/prop-types */
-import classNames from "classnames";
-import { BiError } from "react-icons/bi";
-import { Label } from "../Label";
-import { Divider } from "../Divider";
-import { TextField } from "../TextField";
-import { Controller } from "react-hook-form";
-import { IoChevronDownOutline } from "react-icons/io5";
-import { Fragment, useEffect, useState } from "react";
-import searchIcon from "../../assets/icons/search.svg";
+import classNames from "classnames"
+import { BiError } from "react-icons/bi"
+import { Label } from "../Label"
+import { Divider } from "../Divider"
+import { TextField } from "../TextField"
+import { Controller } from "react-hook-form"
+import { IoChevronDownOutline } from "react-icons/io5"
+import { Fragment, useEffect, useState } from "react"
+import searchIcon from "../../assets/icons/search.svg"
 
 export const Select = ({
-  control,
-  label,
-  inputClassName,
-  optionClassName,
-  containerClassName,
-  options = [],
-  required = false,
-  divider = false,
-  userGuide,
-  educationalContent,
-  questionKey,
-  errors = {},
-  errorMessage = "پر کردن این قسمت الزامیست.",
-  dividerClassName,
-  archive,
-  errorIcon,
-  onChange,
-  value,
-  labelClassName,
-  en,
-  register,
-  search,
-  labelMore,
-  ...props
+    control,
+    label,
+    inputClassName,
+    optionClassName,
+    containerClassName,
+    options = [],
+    required = false,
+    divider = "center",
+    userGuide,
+    educationalContent,
+    questionKey,
+    errors = {},
+    errorMessage = "پر کردن این قسمت الزامیست.",
+    dividerClassName,
+    archive,
+    errorIcon,
+    onChange,
+    value,
+    labelClassName,
+    en,
+    register,
+    search,
+    labelMore,
+    ...props
 }) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [searchValue, setSearchValue] = useState("");
-  const isError = !!errors[questionKey] && !value;
-  const hasController = !!Controller && !!control;
+    const [isOpen, setIsOpen] = useState(false)
+    const [searchValue, setSearchValue] = useState("")
+    const isError = !!errors[questionKey] && !value
+    const hasController = !!Controller && !!control
 
-  const defaultOptions = [
-    { label: "لطفا یک گزینه را انتخاب کنید.", value: "" },
-    ...options,
-  ];
+    const defaultOptions = [
+        { label: "لطفا یک گزینه را انتخاب کنید.", value: "" },
+        ...options,
+    ]
 
-  const renderSelect = ({ field }) => (
-    <select
-      id={questionKey}
-      className={classNames(
-        inputClassName,
-        "text-3xs md:text-2xs lg:text-xs rounded border-[0.5px] border-solid border-black hover:border-success  bg-formItemInput font-400  py-1 lg:py-1.5 px-1 lg:px-1.5  !ring-0 hover:bg-white cursor-pointer outline-none"
-      )}
-      onChange={(e) => {
-        const selectedValue =
-          e.target.value === "لطفا یک گزینه را انتخاب کنید."
-            ? undefined
-            : e.target.value;
-        if (field?.onChange) field.onChange(selectedValue);
-        if (onChange) onChange(selectedValue);
-      }}
-      value={field?.value ?? value}
-      ref={field?.ref}
-      dir={en ? "ltr" : "rtl"}
-      {...props}
-    >
-      {defaultOptions.map((option) => (
-        <option
-          key={option.value}
-          value={option.value}
-          className={classNames(
-            "text-2xs lg:text-xs p-2 bg-formItemInput rounded border-[0.5px] border-solid border-black",
-            optionClassName
-          )}
+    const renderSelect = ({ field }) => (
+        <select
+            id={questionKey}
+            className={classNames(
+                inputClassName,
+                "text-3xs md:text-2xs lg:text-xs rounded border-[0.5px] border-solid border-black hover:border-success  bg-formItemInput font-400  py-1 lg:py-1.5 px-1 lg:px-1.5  !ring-0 hover:bg-white cursor-pointer outline-none"
+            )}
+            onChange={(e) => {
+                const selectedValue =
+                    e.target.value === "لطفا یک گزینه را انتخاب کنید."
+                        ? undefined
+                        : e.target.value
+                if (field?.onChange) field.onChange(selectedValue)
+                if (onChange) onChange(selectedValue)
+            }}
+            value={field?.value ?? value}
+            ref={field?.ref}
+            dir={en ? "ltr" : "rtl"}
+            {...props}
         >
-          {option.label}
-        </option>
-      ))}
-    </select>
-  );
+            {defaultOptions.map((option) => (
+                <option
+                    key={option.value}
+                    value={option.value}
+                    className={classNames(
+                        "text-2xs lg:text-xs p-2 bg-formItemInput rounded border-[0.5px] border-solid border-black",
+                        optionClassName
+                    )}
+                >
+                    {option.label}
+                </option>
+            ))}
+        </select>
+    )
 
-  const labelDirectionStyle = {
-    center: "label-center",
-    right: "label-right",
-    left: "label-left",
-  };
-
-  useEffect(() => {
-    if (register && required) {
-      register(questionKey, {
-        required,
-      });
+    const labelDirectionStyle = {
+        center: "label-center",
+        right: "label-right",
+        left: "label-left",
     }
-  }, []);
 
-  return (
-    <Fragment>
-      {isOpen ? (
-        <div
-          onClick={() => setIsOpen(false)}
-          className="fixed top-0 left-0 h-screen w-screen z-[999]"
-        ></div>
-      ) : null}
-      <div
-        className={classNames(
-          "bg-formItem w-full flex flex-col relative p-2 rounded",
-          containerClassName,
-          {
-            "field-error": isError,
-            "z-[1000]": isOpen,
-          }
-        )}
-        style={{
-          boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.15)",
-        }}
-      >
-        <Label
-          className={classNames(labelClassName, labelDirectionStyle[divider])}
-          userGuide={userGuide}
-          educationalContent={educationalContent}
-          archive={archive ? { ...archive, questionKey } : false}
-          label={label}
-          required={required}
-          en={en}
-          more={labelMore}
-        />
-        {divider && (
-          <Divider
-            className={classNames(dividerClassName)}
-            position={divider}
-          />
-        )}
-        {search ? (
-          <div className="relative w-full">
-            <div
-              className="flex items-center px-1 justify-between w-full rounded border-[0.5px] border-black py-0.5 md:py-[3px] lg:py-[5px] bg-formItemInput cursor-pointer select-none"
-              onClick={() => setIsOpen(!isOpen)}
-            >
-              <span
-                className={
-                  "text-3xs md:text-2xs lg:text-xs xl:text-[13px] font-400"
-                }
-              >
-                {value
-                  ? defaultOptions?.find((o) => o.value == value)?.label
-                  : "لطفا یک گزینه را انتخاب کنید."}
-              </span>
-              <IoChevronDownOutline
-                className={` ${isOpen ? "rotate-180 transition-all" : ""}`}
-              />
-            </div>
+    useEffect(() => {
+        if (register && required) {
+            register(questionKey, {
+                required,
+            })
+        }
+    }, [])
+
+    return (
+        <Fragment>
             {isOpen ? (
-              <div className="absolute left-0 bottom-0 translate-y-full flex flex-col rounded-b w-full transition-all z-10">
-                <div className="bg-formItem2 p-1 rounded w-full">
-                  <TextField
-                    icon={<img src={searchIcon} alt="جستجو" loading="lazy" />}
-                    placeholder="حداقل ۲ حرف از نام دارو را وارد کنید."
-                    value={searchValue}
-                    onChange={(e) => setSearchValue(e.target.value)}
-                  />
-                </div>
-                {searchValue?.length >= 2 ? (
-                  options.filter((o) => o.label.includes(searchValue))
-                    ?.length ? (
-                    <ul className="flex flex-col divide-y divide-solid divide-gray-200 max-h-[200px] overflow-y-scroll bg-[#f7f7f7] shadow-md">
-                      {options
-                        .filter((o) => o.label.includes(searchValue))
-                        .map((o) => (
-                          <li
-                            key={o.label}
-                            onClick={() => {
-                              onChange(o.value);
-                              setIsOpen(false);
-                            }}
-                            className="p-2 text-3xs md:text-2xs lg:text-xs font-500 hover:bg-gray-200 cursor-pointer"
-                          >
-                            {o.label}
-                          </li>
-                        ))}
-                    </ul>
-                  ) : (
-                    <span className="py-3 px-2 text-3xs md:text-2xs lg:text-xs font-500 bg-gray-200">
-                      موردی یافت نشد.
-                    </span>
-                  )
-                ) : null}
-              </div>
+                <div
+                    onClick={() => setIsOpen(false)}
+                    className="fixed top-0 left-0 h-screen w-screen z-[999]"
+                ></div>
             ) : null}
-          </div>
-        ) : hasController ? (
-          <Controller
-            control={control}
-            name={questionKey}
-            rules={{ required }}
-            render={({ field }) => renderSelect({ field })}
-          />
-        ) : (
-          renderSelect({ field: { value, onChange, ref: null } })
-        )}
+            <div
+                className={classNames(
+                    "bg-formItem w-full flex flex-col relative p-2 rounded",
+                    containerClassName,
+                    {
+                        "field-error": isError,
+                        "z-[1000]": isOpen,
+                    }
+                )}
+                style={{
+                    boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.15)",
+                }}
+            >
+                <Label
+                    className={classNames(
+                        labelClassName,
+                        labelDirectionStyle[divider]
+                    )}
+                    userGuide={userGuide}
+                    educationalContent={educationalContent}
+                    archive={archive ? { ...archive, questionKey } : false}
+                    label={label}
+                    required={required}
+                    en={en}
+                    more={labelMore}
+                />
+                {divider && (
+                    <Divider
+                        className={classNames(dividerClassName)}
+                        position={divider}
+                    />
+                )}
+                {search ? (
+                    <div className="relative w-full">
+                        <div
+                            className="flex items-center px-1 justify-between w-full rounded border-[0.5px] border-black py-0.5 md:py-[3px] lg:py-[5px] bg-formItemInput cursor-pointer select-none"
+                            onClick={() => setIsOpen(!isOpen)}
+                        >
+                            <span
+                                className={
+                                    "text-3xs md:text-2xs lg:text-xs xl:text-[13px] font-400"
+                                }
+                            >
+                                {value
+                                    ? defaultOptions?.find(
+                                          (o) => o.value == value
+                                      )?.label
+                                    : "لطفا یک گزینه را انتخاب کنید."}
+                            </span>
+                            <IoChevronDownOutline
+                                className={` ${
+                                    isOpen ? "rotate-180 transition-all" : ""
+                                }`}
+                            />
+                        </div>
+                        {isOpen ? (
+                            <div className="absolute left-0 bottom-0 translate-y-full flex flex-col rounded-b w-full transition-all z-10">
+                                <div className="bg-formItem2 p-1 rounded w-full">
+                                    <TextField
+                                        icon={
+                                            <img
+                                                src={searchIcon}
+                                                alt="جستجو"
+                                                loading="lazy"
+                                            />
+                                        }
+                                        placeholder="حداقل ۲ حرف از نام دارو را وارد کنید."
+                                        value={searchValue}
+                                        onChange={(e) =>
+                                            setSearchValue(e.target.value)
+                                        }
+                                    />
+                                </div>
+                                {searchValue?.length >= 2 ? (
+                                    options.filter((o) =>
+                                        o.label.includes(searchValue)
+                                    )?.length ? (
+                                        <ul className="flex flex-col divide-y divide-solid divide-gray-200 max-h-[200px] overflow-y-scroll bg-[#f7f7f7] shadow-md">
+                                            {options
+                                                .filter((o) =>
+                                                    o.label.includes(
+                                                        searchValue
+                                                    )
+                                                )
+                                                .map((o) => (
+                                                    <li
+                                                        key={o.label}
+                                                        onClick={() => {
+                                                            onChange(o.value)
+                                                            setIsOpen(false)
+                                                        }}
+                                                        className="p-2 text-3xs md:text-2xs lg:text-xs font-500 hover:bg-gray-200 cursor-pointer"
+                                                    >
+                                                        {o.label}
+                                                    </li>
+                                                ))}
+                                        </ul>
+                                    ) : (
+                                        <span className="py-3 px-2 text-3xs md:text-2xs lg:text-xs font-500 bg-gray-200">
+                                            موردی یافت نشد.
+                                        </span>
+                                    )
+                                ) : null}
+                            </div>
+                        ) : null}
+                    </div>
+                ) : hasController ? (
+                    <Controller
+                        control={control}
+                        name={questionKey}
+                        rules={{ required }}
+                        render={({ field }) => renderSelect({ field })}
+                    />
+                ) : (
+                    renderSelect({ field: { value, onChange, ref: null } })
+                )}
 
-        {isError ? (
-          <span className="error">
-            {errorIcon || <BiError className="text-xs lg:text-base" />}
-            {errorMessage}
-          </span>
-        ) : null}
-      </div>
-    </Fragment>
-  );
-};
+                {isError ? (
+                    <span className="error">
+                        {errorIcon || (
+                            <BiError className="text-xs lg:text-base" />
+                        )}
+                        {errorMessage}
+                    </span>
+                ) : null}
+            </div>
+        </Fragment>
+    )
+}
