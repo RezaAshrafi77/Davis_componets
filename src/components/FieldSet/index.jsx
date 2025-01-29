c93e2c096254297824defedc5d2b935999bf530c
RezaAshrafi77 <ashrafimirreza98@gmail.com>
2025-01-29 09:34:03 +0330

    update 17 files

--- src/components/FieldSet/index.jsx
+++ src/components/FieldSet/index.jsx
@@ -1,56 +1,56 @@
-import classNames from "classnames";
-import styles from "./styles.module.css";
+import classNames from "classnames"
+import styles from "./styles.module.css"
 
 export const FieldSet = ({
-  title,
-  children,
-  containerClassName,
-  className,
-  titleClassName,
-  en,
-  gradientBorder = true,
+    title,
+    children,
+    containerClassName,
+    className,
+    titleClassName,
+    en,
+    gradientBorder = true,
 }) => (
-  <fieldset
-    className={classNames(
-      styles.fieldset,
-      containerClassName,
-      !gradientBorder ? "!border-none" : ""
-    )}
-    role="group"
-    // dir={en ? "ltr" : ""}
-    aria-labelledby={title ? "fieldset-legend" : undefined}
-  >
-    {/* {gradientBorder ? (
+    <fieldset
+        className={classNames(
+            styles.fieldset,
+            containerClassName,
+            !gradientBorder ? "!border-none" : ""
+        )}
+        role="group"
+        // dir={en ? "ltr" : ""}
+        aria-labelledby={title ? "fieldset-legend" : undefined}
+    >
+        {/* {gradientBorder ? (
       <img
         src={borderSVG}
         className="absolute left-0 -top-3.5 min-w-full min-h-full"
       />
     ) : null} */}
-    {title && (
-      <legend
-        id="fieldset-legend"
-        className={classNames(
-          "text-xs relative font-700 z-[1] lg:text-sm mr-6 lg:mr-8 text-[#3D0C02] rounded py-0 px-2",
-          en ? styles.enLegend : "",
-          titleClassName,
-          !gradientBorder ? "translate-y-1/2 bg-white" : ""
+        {title && (
+            <legend
+                id="fieldset-legend"
+                className={classNames(
+                    "text-xs relative font-700 z-[1] lg:text-sm mr-6 lg:mr-8 text-[#3D0C02] rounded py-0 px-2",
+                    en ? styles.enLegend : "",
+                    titleClassName,
+                    !gradientBorder ? "translate-y-1/2 bg-white" : ""
+                )}
+            >
+                {title}
+            </legend>
         )}
-      >
-        {title}
-      </legend>
-    )}
-    <div
-      className={classNames(
-        className,
-        gradientBorder
-          ? title
-            ? styles.borderImageByTitle
-            : styles.borderImage
-          : styles.border,
-        "relative w-full px-3 md:px-4 py-4 lg:px-8 lg:pt-6"
-      )}
-    >
-      {children}
-    </div>
-  </fieldset>
-);
+        <div
+            className={classNames(
+                className,
+                gradientBorder
+                    ? title
+                        ? styles.borderImageByTitle
+                        : styles.borderImage
+                    : styles.border,
+                "relative w-full px-3 md:px-4 py-4 lg:px-8 lg:pt-6"
+            )}
+        >
+            {children}
+        </div>
+    </fieldset>
+)
